15fdd4ccaf58ffb525b12e98951ccf984871af61
Tomas Barton <barton.tomas@gmail.com>
2015-10-01 15:17:52 +0200

    move junit tests to appropriate directory

--- src/test/java/edu/wlu/cs/levy/CG/KDTreeTest.java
+++ src/test/java/edu/wlu/cs/levy/CG/KDTreeTest.java
@@ -1,38 +1,52 @@
+/*
+ * Copyright (C) 2015 Simon Levy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package edu.wlu.cs.levy.CG;
 
-
-/*
-  written by MSL for SpeedDate
-*/
 import java.util.List;
-
-
-import org.junit.Test;
-import org.junit.Ignore;
 import org.junit.Assert;
+import org.junit.Test;
 
-public class KDTests {
+/**
+ *
+ * @author MSL
+ */
+public class KDTreeTest {
+
     static java.util.Random rand = new java.util.Random();
 
     static double[] makeSample(int dims) {
-        double [] rv = new double [dims];
-        for (int j=0; j < dims; ++j) {
+        double[] rv = new double[dims];
+        for (int j = 0; j < dims; ++j) {
             rv[j] = rand.nextDouble();
         }
         return rv;
     }
 
-    static double distSquared(double [] p0, double [] p1) {
+    static double distSquared(double[] p0, double[] p1) {
         double rv = 0;
-        for (int i=0; i < p0.length; i++) {
+        for (int i = 0; i < p0.length; i++) {
             double diff = p0[i] - p1[i];
             rv += (diff * diff);
         }
         return rv;
     }
 
-
-    @Test public void testNearestNeighborList() {
+    @Test
+    public void testNearestNeighborList() {
         NearestNeighborList<String> nnl = new NearestNeighborList<String>(3);
         nnl.insert("A", 3.0);
         nnl.insert("B", 2.0);
@@ -46,22 +60,23 @@ public class KDTests {
         Assert.assertEquals("D", nnl.removeHighest());
     }
 
-    @Test public void testNearestNeighbor() throws KDException {
+    @Test
+    public void testNearestNeighbor() throws KDException {
         int dims = 3;
         int samples = 300;
         KDTree<Integer> kt = new KDTree<Integer>(dims);
-        double [] targ = makeSample(dims);
+        double[] targ = makeSample(dims);
 
         int min_index = 0;
         double min_value = Double.MAX_VALUE;
-        for (int i=0; i<samples; ++i) {
-            double [] keys = makeSample(dims);
+        for (int i = 0; i < samples; ++i) {
+            double[] keys = makeSample(dims);
             kt.insert(keys, new Integer(i));
 
             /*
-              for the purposes of test, we want the nearest EVEN-NUMBERED point 
-            */
-            if ( (i % 2) == 0) {
+             for the purposes of test, we want the nearest EVEN-NUMBERED point
+             */
+            if ((i % 2) == 0) {
                 double dist = distSquared(targ, keys);
                 if (dist < min_value) {
                     min_value = dist;
@@ -70,31 +85,32 @@ public class KDTests {
             }
         }
 
-
         List<Integer> nbrs = kt.nearest(targ, 1, new Checker<Integer>() {
-                public boolean usable(Integer v) {
-                    return (v.intValue() % 2) == 0;
-                }});
-	
+            public boolean usable(Integer v) {
+                return (v.intValue() % 2) == 0;
+            }
+        });
+
         Assert.assertEquals(1, nbrs.size());
         if (nbrs.size() == 1) {
             Assert.assertEquals(min_index, nbrs.get(0).intValue());
         }
     }
 
-    @Test public void testRange() throws KDException {
+    @Test
+    public void testRange() throws KDException {
         int dims = 2;
         KDTree<Object> kt = new KDTree<Object>(dims);
-        double [] p0 = { 0.5, 0.5 };
-        double [] p1 = { 0.65, 0.5 };
-        double [] p2 = { 0.75, 0.5 };
+        double[] p0 = {0.5, 0.5};
+        double[] p1 = {0.65, 0.5};
+        double[] p2 = {0.75, 0.5};
 
         kt.insert(p0, new Object());
         kt.insert(p1, new Object());
         kt.insert(p2, new Object());
 
-        double [] lower = { 0.25, 0.3 };
-        double [] upper = { 0.7, 0.6 };
+        double[] lower = {0.25, 0.3};
+        double[] upper = {0.7, 0.6};
 
         List<Object> rv = kt.range(lower, upper);
         Assert.assertEquals(2, rv.size());
@@ -104,16 +120,17 @@ public class KDTests {
         Assert.assertEquals(1, rv.size());
     }
 
-    @Test public void testSearch() throws KDException {
+    @Test
+    public void testSearch() throws KDException {
         int dims = 3;
         int samples = 300;
         KDTree<Object> kt = new KDTree<Object>(dims);
-        double [] targ = makeSample(dims);
+        double[] targ = makeSample(dims);
         Object treasure = new Object();
         kt.insert(targ, treasure);
 
-        for (int i=0; i<samples; ++i) {
-            double [] keys = makeSample(dims);
+        for (int i = 0; i < samples; ++i) {
+            double[] keys = makeSample(dims);
             kt.insert(keys, new Integer(i));
         }
 
@@ -126,26 +143,28 @@ public class KDTests {
 
     }
 
-    @Test public void testDelete() throws KDException {
+    @Test
+    public void testDelete() throws KDException {
         int dims = 3;
         KDTree<Object> kt = new KDTree<Object>(dims);
-        double [] targ = makeSample(dims);
+        double[] targ = makeSample(dims);
         kt.insert(targ, new Object());
         kt.delete(targ);
         try {
             kt.delete(targ);
             Assert.assertFalse(true);
-        } catch (edu.wlu.cs.levy.CG.KeyMissingException e) { 
+        } catch (edu.wlu.cs.levy.CG.KeyMissingException e) {
             // supposed to be here
         }
         kt.delete(targ, true);
         Assert.assertEquals(0, kt.size());
     }
 
-    @Test public void testEditing() throws KDException {
+    @Test
+    public void testEditing() throws KDException {
         int dims = 3;
         KDTree<Object> kt = new KDTree<Object>(dims);
-        double [] targ = makeSample(dims);
+        double[] targ = makeSample(dims);
 
         Object p1 = "p1";
         Object p2 = "p2";
@@ -153,7 +172,7 @@ public class KDTests {
         try {
             kt.insert(targ, p2);
             Assert.assertFalse(true);
-        } catch (edu.wlu.cs.levy.CG.KeyDuplicateException e) { 
+        } catch (edu.wlu.cs.levy.CG.KeyDuplicateException e) {
             // supposed to be here
         }
 
@@ -164,4 +183,5 @@ public class KDTests {
         found = kt.search(targ);
         Assert.assertSame(p2, found);
     }
+
 }
